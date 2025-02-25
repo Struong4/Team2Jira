d8196f07d1c48c6f84f3c014a4c9400166af416f
haimgia2 <158623446+haimgia2@users.noreply.github.com>
2025-02-25 09:15:18 -0500

    adding load and delete scripts for sql database

---
+++ drop_all.sql
@@ -0,0 +1,15 @@
+USE Inventory;
+
+SET SQL_SAFE_UPDATES = 0;
+
+-- drops all tables
+DROP TABLE IF EXISTS buys;
+DROP TABLE IF EXISTS updates;
+DROP TABLE IF EXISTS restock;
+
+DROP TABLE IF EXISTS origin;
+DROP TABLE IF EXISTS category;
+
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS staff;
