915270b930cb2701bb0f42d85dffd4c54fd6e0a8
barella1umbc <158183459+barella1umbc@users.noreply.github.com>
2025-04-06 14:09:14 -0400

    added SQL/crud utility to item class

---
+++ inventory database/item_class.py
@@ -0,0 +1,109 @@
+
+#class for an item within the database
+#attribute name: the string name of the item
+#attribute price: the double price of the item
+#attribute quantity: the int quantity of the item
+#attribute weight: the double weight of item per one unit
+
+from crud import addNewItem
+from crud import removeItem
+
+class Item:
+    def __init__(self, item_id, name, price, quantity, weight, description, origin, category, quantity_limit):
+        self.item_id = str(item_id)  #unique ID
+        self.name = name  #item Name
+        self.price = float(price)  #price per unit
+        self.quantity = int(quantity)  #available Quantity
+        self.weight = float(weight)  #weight per unit
+        self.description = description  #item Description
+        self.origins = origin  #origins
+        self.category = category  #categories
+        self.quantity_limit = int(quantity_limit)  #quantity limit per user
+        
+        print(f"Item successfully created!\n"
+              f"----------------------------------\n"
+              f"Item ID: {self.item_id}\n"
+              f"Name: {self.name}\n"
+              f"Price (per unit): ${self.price:.2f}\n"
+              f"Current Quantity: {self.quantity}\n"
+              f"Quantity Limit: {self.quantity_limit}\n"
+              f"Weight (per unit): {self.weight} kg\n"
+              f"Description: {self.description}\n"
+              f"Origins: {self.origins}\n"
+              f"Category: {self.category}\n"
+              f"----------------------------------\n")
+        
+        # create item in sql database
+        addNewItem(
+            item_id=self.item_id,
+            item_name=self.name,
+            weight_lbs=self.weight,
+            quantity=self.quantity,
+            price=self.price,
+            descript=self.description,
+            quantity_limit=self.quantity_limit,
+            origins=self.origins,
+            categories=self.category
+        )
+
+
+    def __del__(self):
+        removeItem(self.item_id) #dwlete item from sql database
+        print(f'Item removed: {self.name} (ID: {self.item_id})\n')
+        
+
+    #accessors
+    def getItemID(self):
+        return self.item_id
+
+    def getName(self):
+        return self.name
+
+    def getPrice(self):
+        return self.price
+
+    def getQuantity(self):
+        return self.quantity
+
+    def getWeight(self):
+        return self.weight
+
+    def getDescription(self):
+        return self.description
+
+    def getOrigin(self):
+        return self.origin
+
+    def getCategory(self):
+        return self.category
+
+    def getQuantityLimit(self):
+        return self.quantity_limit
+
+    #mutators
+    def setItemID(self, new_id):
+        self.item_id = str(new_id)
+
+    def setName(self, new_name):
+        self.name = new_name
+
+    def setPrice(self, new_price):
+        self.price = float(new_price)
+
+    def setQuantity(self, new_quantity):
+        self.quantity = int(new_quantity)
+
+    def setWeight(self, new_weight):
+        self.weight = float(new_weight)
+
+    def setDescription(self, new_description):
+        self.description = new_description
+
+    def setOrigin(self, new_origin):
+        self.origin = new_origin if isinstance(new_origin, list) else [new_origin]
+
+    def setCategory(self, new_category):
+        self.category = new_category if isinstance(new_category, list) else [new_category]
+
+    def setQuantityLimit(self, new_limit):
+        self.quantity_limit = int(new_limit)
